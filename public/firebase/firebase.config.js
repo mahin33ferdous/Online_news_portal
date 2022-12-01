2222e3bcd6753dfaa6eee9f835a821cc9ac9e23e
asad <asaduzzaman@lapasar.com>
2022-12-01 13:10:57 +0600

    project set up with firebase routing and boostrap

---
+++ public/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCVMOA0WnJIgxDUaiq05B41Al3ZMh8l9dU",
+  authDomain: "online-news-portal-833fd.firebaseapp.com",
+  projectId: "online-news-portal-833fd",
+  storageBucket: "online-news-portal-833fd.appspot.com",
+  messagingSenderId: "890997409445",
+  appId: "1:890997409445:web:517b76478cd980d70a2f4c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/Layouts/Main.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+
+const Main = () => {
+
+    <h1> side bar</h1>
+    return (
+
+       
+        <Container>
+          <Row>
+            <Col>
+            <h1> side bar</h1>
+            </Col>
+            <Col>
+            <h1> main</h1>
+            </Col>
+            <Col>
+            <h1> right side bar</h1>
+            </Col>
+          </Row>
+        </Container>
+    );
+};
+
+export default Main;
